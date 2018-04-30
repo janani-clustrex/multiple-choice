f10632af7cc9de791db8a5c5b8b9c51a260a7b63
janani-clustrex <janani28203@gmail.com>
2018-04-30 17:35:56 +0530

    multiple choice starting

---
+++ choice.js
@@ -0,0 +1,113 @@
+var myJSON = [
+    {
+        "questionsId": "1",
+        "question": "Which is the highest mountain in the world?",
+        "options": {
+            "option1": "Himalayas",
+            "option2": "mount k2",
+            "option3": "Mount Everest",
+            "option4": "mount cook"
+        },
+        "answer1": "option3"
+    },
+    {
+        "questionsId": "2",
+        "question": "write the full form of https?",
+        "options": {
+            "option1": "Himalayas",
+            "option2": "mount k2",
+            "option3": "Mount Everest",
+            "option4": "mount cook"
+        },
+        "answer2": "option3"
+    },
+      {
+        "questionsId": "1",
+        "question": "Which is the highest mountain in the world?",
+        "options": {
+            "option1": "Himalayas",
+            "option2": "mount k2",
+            "option3": "Mount Everest",
+            "option4": "mount cook"
+        },
+        "answer1": "option3"
+    },
+      {
+        "questionsId": "1",
+        "question1": "Which is the highest mountain in the world?",
+        "options": {
+            "option1": "Himalayas",
+            "option2": "mount k2",
+            "option3": "Mount Everest",
+            "option4": "mount cook"
+        },
+        "answer1": "option3"
+    },
+      {
+        "questionsId": "1",
+        "question1": "Which is the highest mountain in the world?",
+        "options": {
+            "option1": "Himalayas",
+            "option2": "mount k2",
+            "option3": "Mount Everest",
+            "option4": "mount cook"
+        },
+        "answer1": "option3"
+    },
+      {
+        "questionsId": "1",
+        "question1": "Which is the highest mountain in the world?",
+        "options": {
+            "option1": "Himalayas",
+            "option2": "mount k2",
+            "option3": "Mount Everest",
+            "option4": "mount cook"
+        },
+        "answer1": "option3"
+    },
+      {
+        "questionsId": "1",
+        "question1": "Which is the highest mountain in the world?",
+        "options": {
+            "option1": "Himalayas",
+            "option2": "mount k2",
+            "option3": "Mount Everest",
+            "option4": "mount cook"
+        },
+        "answer1": "option3"
+    },
+      {
+        "questionsId": "1",
+        "question1": "Which is the highest mountain in the world?",
+        "options": {
+            "option1": "Himalayas",
+            "option2": "mount k2",
+            "option3": "Mount Everest",
+            "option4": "mount cook"
+        },
+        "answer1": "option3"
+    },
+      {
+        "questionsId": "1",
+        "question1": "Which is the highest mountain in the world?",
+        "options": {
+            "option1": "Himalayas",
+            "option2": "mount k2",
+            "option3": "Mount Everest",
+            "option4": "mount cook"
+        },
+        "answer1": "option3"
+    },
+      {
+        "questionsId": "1",
+        "question1": "Which is the highest mountain in the world?",
+        "options": {
+            "option1": "Himalayas",
+            "option2": "mount k2",
+            "option3": "Mount Everest",
+            "option4": "mount cook"
+        },
+        "answer1": "option3"
+    },
+
+];
