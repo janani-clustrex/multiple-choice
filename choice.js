a41f35f5b1f3f07b10f360f267bdfd762e6a80e4
sangeetha-clustrex <jsangeetha@clustrex.com>
2018-05-03 16:35:11 +0530

    checking answers

--- choice.js
+++ choice.js
@@ -14,7 +14,7 @@ var MyJSON = [
         "option2": "mycelin sheath",
         "option3": "axon",
         "option4": "nerve ending",
-        "answer": "option31"
+        "answer": "option11"
     },
     {
         "question": "Which part of brain controls voluntary action? ",
@@ -22,7 +22,7 @@ var MyJSON = [
         "option2": "hind brain",
         "option3": "fore brain",
         "option4": "none ofthese",
-        "answer": "option31"
+        "answer": "option11"
     },
     // {
     //     "question": "Who sung vandhe mataram ?",
@@ -57,75 +57,101 @@ var MyJSON = [
     //     "answer": "option21"
     // },
     // {
-    //     "question":"What is the currency of french indochina?",
-    //     "option1":"rupee",
-    //     "option2":"yen",
-    //     "option3":"dollar",
-    //     "option4":"piastre",
-    //     "answer":"option41"
+    //     "question": "What is the currency of french indochina?",
+    //     "option1": "rupee",
+    //     "option2": "yen",
+    //     "option3": "dollar",
+    //     "option4": "piastre",
+    //     "answer": "option41"
     // },
-    //     { 
-    //      "question":"which country of the world has largest hindu population after India?",
-    //      "option1":"nepal",
-    //      "option2":"mauritus",
-    //      "option3":"Bangladesh",
-    //      "option4":"srilanka",
-    //      "answer":"option21"
-    //     },
-    //     {
-    //       "question":"In which country teachers are paid more than other occupations?",
-    //       "option1":"Luxemburg",
-    //       "option2":"switzerland",
-    //       "option3":"Germany",
-    //       "option4":"none of these",
-    //       "answer":"option11"
-    //     },
-       
+    // {
+    //     "question": "which country of the world has largest hindu population after India?",
+    //     "option1": "nepal",
+    //     "option2": "mauritus",
+    //     "option3": "Bangladesh",
+    //     "option4": "srilanka",
+    //     "answer": "option21"
+    // },
+    // {
+    //     "question": "In which country teachers are paid more than other occupations?",
+    //     "option1": "Luxemburg",
+    //     "option2": "switzerland",
+    //     "option3": "Germany",
+    //     "option4": "none of these",
+    //     "answer": "option11"
+    // },
+
 ]
 var questionNo = 0;
-console.log("i am in")
-var selectedOption;
-var score = 0; 
-var x="score:"+score;
-x.style.textAlign="right";
+var selectedOption = "";
+var score = 0;
 function check(element) {
     selectedOption = element.id;
 }
 
 
 function next() {
-    
-
-    
-   if(questionNo == MyJSON.length-2){
-     document.getElementById("next").innerHTML="FINISH";
-   }
-    if(questionNo<MyJSON.length-1){
-    if (selectedOption === MyJSON[questionNo].answer) {
-        score = score + 1;
-        alert("answer is correct  score:" + score);
-        document.getElementById(selectedOption).checked = false;
-        selectedOption="";
+    // if nothing is selcted and next is clicked
+    if (selectedOption === "") {
         questionNo = questionNo + 1;
+    }
+    //checking questions except for last one
+    //change the button from next to finish
+        if (questionNo == MyJSON.length - 2) {
+            document.getElementById("next").innerHTML = "FINISH";
+        }
+        //to excecute all the questions and terminate after  it 
+        if (questionNo < MyJSON.length - 1) {
+            //to check whether the answer is correct or wrong except for last one
+            if (selectedOption === MyJSON[questionNo].answer) {
+                score = score + 1;
+                questionNo = questionNo + 1;
+                console.log('selectedOption', selectedOption)
+                document.getElementById(selectedOption).checked = false;
+                selectedOption = "";
+            }
+            //wrong one
+            else  {
+                questionNo = questionNo + 1;
+                document.getElementById(selectedOption).checked = false;
+                selectedOption = "";
+            }
 
-    }
-    else {
-        alert("answer is wrong score:"+score);
-        console.log(selectedOption)
-        document.getElementById(selectedOption).checked = false;
-         selectedOption="";
-         questionNo = questionNo + 1;
-    }
-}
-    else{
-        alert("game over!!!");
-    }
-    
-    console.log("value", document.getElementById("option1"))
-    document.getElementById("question").innerHTML = MyJSON[questionNo].question;
-    document.getElementById("option1").innerHTML = MyJSON[questionNo].option1;
-    document.getElementById("option2").innerHTML = MyJSON[questionNo].option2;
-    document.getElementById("option3").innerHTML = MyJSON[questionNo].option3;
-    document.getElementById("option4").innerHTML = MyJSON[questionNo].option4;
+            //to go to next question
+            document.getElementById("question").innerHTML = MyJSON[questionNo].question;
+            document.getElementById("option1").innerHTML = MyJSON[questionNo].option1;
+            document.getElementById("option2").innerHTML = MyJSON[questionNo].option2;
+            document.getElementById("option3").innerHTML = MyJSON[questionNo].option3;
+            document.getElementById("option4").innerHTML = MyJSON[questionNo].option4;
+        }
+        //to check last question
+        else if (questionNo == MyJSON.length - 1) {
+            
+            //correct one
+            if (selectedOption === MyJSON[questionNo].answer) {
+                score = score + 1;
+                questionNo = questionNo + 1;
+                document.getElementById(selectedOption).checked = false;
+                selectedOption = "";
 
-}
+            }
+            //wrong one
+            else {
+                questionNo = questionNo + 1;
+                document.getElementById(selectedOption).checked = false;
+                selectedOption = "";
+            }//to make the questions disappear from the screen
+            var x = document.getElementById("mcq");
+            x.style.display = "none";
+            var y = document.getElementById("next");
+            y.style.display = "none";
+        }
+
+        else {
+            //after last question,to give score
+           
+            document.getElementById("Score").innerText = "score:" + score + "\nwell done!!! try again";
+            document.getElementById("Score").style.fontsize = "12em";
+        }
+    }
+
